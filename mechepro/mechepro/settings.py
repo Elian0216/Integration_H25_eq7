8cd958c8c87407b1057d9ea1d8147d393a7a800f
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-02-26 16:09:42 -0500

    En cours de changement de la page home

--- mechepro/mechepro/settings.py
+++ mechepro/mechepro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^-4--3d*ib7ywg^me=be91iiynracx^s_yiznf2@m&ehg=)%&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
