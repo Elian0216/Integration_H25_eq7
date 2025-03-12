435843db27748066933e9d86dccd2dcbf07108a8
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-03-12 17:49:45 -0400

    Avancement sur la page de graphique, insertion du choicefield
    Courage l'équipe MèchePro!

--- mechepro/home/analyseFinanciere/yahooFinance.py
+++ mechepro/home/analyseFinanciere/yahooFinance.py
@@ -1,4 +1,7 @@
 import yfinance as yf
+import pandas as pd
+from django.shortcuts import render
+
 
 def get_donnees_stock(ticker, period="1y"):
     stock_data = yf.download(ticker, period=period)
@@ -17,3 +20,22 @@ def get_donnees_stock(ticker, period="1y"):
 # Pour tester le script directement
 if __name__ == "__main__":
     get_donnees_stock("AAPL")
+
+
+def get_all_stock_symbols():
+    # Télécharger les composants du S&P 500
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    table = pd.read_html(url)
+    df = table[0]
+    symbols = df['Symbol'].tolist()
+    
+    # Vérifier que les symboles sont valides avec yfinance
+    valid_symbols = []
+    for symbol in symbols[:50]:
+        try:
+            stock = yf.Ticker(symbol)
+            info = stock.info
+            valid_symbols.append((symbol, info.get('longName', 'Nom inconnu')))
+        except Exception as e:
+            print(f"Erreur avec le symbole {symbol}: {e}")
+    return  valid_symbols
