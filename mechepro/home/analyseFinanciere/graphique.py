e1e93ac3a691e3e9d82ed571f1693a79d3aeb54c
Valère Bardon <comptedecode@gmail.com>
2025-03-19 16:10:10 -0400

    Ajouté du javascript pour pouvoir dynamiquement changer les proportions du graphique lors des zooms, dezoom et changement de période.

--- mechepro/home/analyseFinanciere/graphique.py
+++ mechepro/home/analyseFinanciere/graphique.py
@@ -82,5 +82,3 @@ def generer_graphique(request):
 
     return render(request, "page_analyse.html", {"graph_html": graph_html, "symbols": get_all_stock_symbols()})	
 
-
-# BANGER POUR RESIZE : document.getElementsByClassName('js-plotly-plot')[0].on('plotly_relayout', function(data) {console.log("fsafd")})
