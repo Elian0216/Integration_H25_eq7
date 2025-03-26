e3189f14a9f7af8ff83aebbdd8ee1e985c5cf649
Valère Bardon <comptedecode@gmail.com>
2025-03-26 17:19:35 -0400

    ajouté un peu de couleurs au graphique

--- mechepro/home/analyseFinanciere/graphique.py
+++ mechepro/home/analyseFinanciere/graphique.py
@@ -5,6 +5,9 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from django.shortcuts import render
 
+COULEUR_TEXTE = "white"
+COULEUR_FOND = '#181C14'
+COULEUR_FOND_GRAPHE ='#3C3D37'
 
 def generer_graphique(request):
     # Test avec AAPL
@@ -12,8 +15,8 @@ def generer_graphique(request):
     stock_data = get_donnees_stock(ticker, "5y")
 
     # Creation du graphique
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1,
-                    subplot_titles=('Candlestick Chart', 'RSI'), row_heights=[0.7, 0.3])
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.15,
+                    subplot_titles=('Candlestick Chart', ''), row_heights=[0.7, 0.3])
     
 
     # Création du sous-graphique pour les bougies
@@ -47,7 +50,7 @@ def generer_graphique(request):
             line=dict(color="blue"),
         ),
         row=2,
-        col=1
+        col=1,
     )
 
     # Changer la taille du sous-graphique
@@ -56,7 +59,6 @@ def generer_graphique(request):
     # Configuration du graphique
     fig.update_layout(
         title=f"{ticker} Stock Price (1 Year)",
-        xaxis_title="Date",
         yaxis_title="Price (USD)",
         dragmode="pan", # Mode de déplacement dans le graphique
         xaxis=dict(
@@ -81,8 +83,14 @@ def generer_graphique(request):
             # autorange=True,
             # fixedrange=False,
         ),
-
+        paper_bgcolor=COULEUR_FOND, 
+        plot_bgcolor=COULEUR_FOND_GRAPHE,
+        font=dict(color=COULEUR_TEXTE),
     )
+
+    fig.update_xaxes(tickfont=dict(color=COULEUR_TEXTE))
+    fig.update_yaxes(tickfont=dict(color=COULEUR_TEXTE))
+    fig.update_layout(legend=dict(font=dict(color=COULEUR_TEXTE)))
 
     # fig.add_trace(
     #     go.Scatter(
