4593a04a1f93cc2833b6aa9e0c8343eefcaf6705
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-04-02 16:02:23 -0400

    Changement accueil

--- frontend/src/components/retour.tsx
+++ frontend/src/components/retour.tsx
@@ -3,7 +3,7 @@ export default function Retour() {
   return (
     <div className="flex bg-zinc-50 px-4 py-1 dark:bg-transparent">
       <Link
-        href={"/accueil"}
+        href={"/"}
         className="text-muted-foreground hover:text-accent-foreground block duration-150"
       >
         <span>Retour à la page d'accueil</span>
