28eff06d38f3f50fbfe1342ad3060e5fd0f4dc5f
vlrr <comptedecode@gmail.com>
2025-05-19 13:03:57 -0400

    Changé la taille du div de la barre de navigation à droite dans parametre pour qu'on puisse voir le bouton déconnexion

--- frontend/src/app/analyse/[stock]/page.tsx
+++ frontend/src/app/analyse/[stock]/page.tsx
@@ -4,11 +4,12 @@ import React from 'react'
 
 const StockPage = async ({ params }: any ) => {
   const { stock } = await params;
+  const timeframe = "1d";
   
   return (
     <>
       <section className="min-h-screen flex items-center justify-center W-full">
-        <Graphique symbol={stock} />
+        <Graphique symbol={stock} timeframe={timeframe} />
       </section>
     </>
   )
--- frontend/src/app/parametres/page.tsx
+++ frontend/src/app/parametres/page.tsx
@@ -43,7 +43,7 @@ export default function Parametres() {
 
   return (
     <div className="flex min-h-screen">
-      <aside className="mt-20 flex flex-col justify-between w-1/5 h-screen border-r-2 p-6">
+      <aside className="mt-20 flex flex-col justify-between w-1/5 border-r-2 p-6">
         <div className="space-y-6">
           <h2 className="font-medium lg:text-3xl">Paramètres</h2>
 
@@ -66,18 +66,19 @@ export default function Parametres() {
             </button>
           </AnimatedGroup>
         </div>
-
-        <Button
-          onClick={() => {
-            postFetch(process.env.API_PATH + "deconnexion/", {});
-            window.location.reload();
-          }}
-          variant="destructive"
-          size="sm"
-          className="mt-5 w-full"
-        >
-          Déconnexion
-        </Button>
+        <div className="flex flex-col items-center">
+          <Button
+            onClick={() => {
+              postFetch(process.env.API_PATH + "deconnexion/", {});
+              window.location.reload();
+            }}
+            variant="destructive"
+            size="sm"
+            className="mt-5 w-full"
+          >
+            Déconnexion
+          </Button>
+        </div>
       </aside>
 
       <main className="flex-1 flex items-center justify-center p-6">
